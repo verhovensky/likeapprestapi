d1c48e8c8df66330256eae0ad68e232187a6f1a1
verhovensky <voinywolnyprod@gmail.com>
2021-04-24 13:22:38 +0600

    Added UserViewSet,authentication required

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from authentication.views import RegistrationAPIView, LoginAPIView
+from authentication.views import RegistrationAPIView, LoginAPIView, UserViewSet
 
 urlpatterns = [
     re_path(r'^registration/?$', RegistrationAPIView.as_view(), name='user_registration'),
-    re_path(r'^login/?$', LoginAPIView.as_view(), name='user_login')
+    re_path(r'^login/?$', LoginAPIView.as_view(), name='user_login'),
+    re_path(r'^users/?$', UserViewSet.as_view(), name='users_list')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -56,5 +56,6 @@ class UserViewSet(ListCreateAPIView):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
