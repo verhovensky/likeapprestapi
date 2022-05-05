8c951e9083a637c78355e335a4431868c7255ed5
verhovensky <voinywolnyprod@gmail.com>
2022-05-05 13:47:44 +0300

    publication endpoint fix

--- publications/views.py
+++ publications/views.py
@@ -12,25 +12,24 @@ class PublicationViewSet(viewsets.ModelViewSet,
                          LikedMixin):
     authentication_classes = ((JWTAuthentication,))
     permission_classes = [permissions.IsAuthenticated]
+    queryset = Publication.objects.all()
+    serializer_class = PublicationSerializer
 
     def list(self, request, *args, **kwargs):
-        queryset = Publication.objects.all()
-        serializer_class = PublicationSerializer(queryset, many=True,
+        serializer_class = PublicationSerializer(self.queryset, many=True,
                                                  context={"user": request.user})
         return Response(serializer_class.data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk=None, *args, **kwargs):
-        queryset = Publication.objects.all()
-        single_publication = get_object_or_404(queryset, pk=pk)
+        single_publication = get_object_or_404(self.queryset, pk=pk)
         serializer_class = PublicationSerializer(single_publication, context={"user": request.user})
         return Response(serializer_class.data, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
-        user = self.request.user
         pub_data = self.request.data
         serializer_class = PublicationSerializer(data={"title": pub_data['title'],
-                                               "content": pub_data['content'],
-                                               "author": user.pk})
+                                                       "content": pub_data['content'],
+                                                       "author": self.request.user.pk})
         serializer_class.is_valid(raise_exception=True)
         serializer_class.save()
         headers = self.get_success_headers(serializer_class.data)
